fb23538511965d8f54ce3ec906d72be4992b002a
Alejandro <aleramlar@gmail.com>
2020-11-22 12:04:50 +0100

    V(1.2)

--- Proyecto-Final/src/interfaz/Interfaz.java
+++ Proyecto-Final/src/interfaz/Interfaz.java
@@ -1,5 +1,48 @@
 package interfaz;
 
-public class Interfaz {
+import dominio.*;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.FileWriter;
 
+public class Interfaz {
+	private static String HELP_TEXT = "Texto de ayuda";
+	private static String NOMBRE_FICHERO = "catalogo.txt";
+	
+	private static void inicializarFichero(Catalogo catalogo){
+		try{
+			FileWriter fw = new FileWriter(NOMBRE_FICHERO);
+			fw.write(catalogo.toString());
+			fw.close();
+		} catch (Exception e){
+			System.out.println(e);
+		}
+	}
+	
+	private static Catalogo inicializarLibreta(String nombreFichero){
+		Catalogo libreta = new Catalogo();
+		try{
+			File file = new File(nombreFichero);
+			Scanner sc = new Scanner(file);
+			while(sc.hasNext()){
+				String nombre = sc.next();
+				String nTel = sc.next();
+				Zapatilla zapatilla = new Zapatilla(modelo, marca, talla);
+				libreta.annadirZapatilla(zapatilla);
+			}
+			sc.close();
+		} catch (FileNotFoundException e){
+			inicializarFichero(libreta);
+		} catch (Exception e){
+			System.out.println(e);
+		}
+		return libreta;
+	}
+	
+	
+	
+	
+	
+	
 }
