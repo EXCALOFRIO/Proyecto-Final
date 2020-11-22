9645d95c005c5d14739430ecaf43b747973933f0
Alejandro <aleramlar@gmail.com>
2020-11-22 13:34:31 +0100

    V(1.3)

--- Proyecto-Final/src/aplicacion/Principal.java
+++ Proyecto-Final/src/aplicacion/Principal.java
@@ -1,5 +1,12 @@
 package aplicacion;
+import interfaz.Interfaz;
+public class Principal{
 
-public class Principal {
-	
+	public static void main(String[] args){
+		String sentencia = "";
+		for(int i = 0; i< args.length; i++){
+			sentencia += args[i] + " ";
+		}
+		//Interfaz.procesarPeticion(sentencia);
+	}
 }
